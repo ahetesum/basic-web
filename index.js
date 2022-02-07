7368cd0d873a82ba17c4c43a059b21bc43a4700c
unknown <mail.ahliwasum@gmail.com>
2022-02-07 10:37:08 +0530

    count javascript javascript

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 alert('I am Javascript')
 console.log('This is also javascript');
+
+var count=0;
+
+count=count+1;
+
+console.log('The value of count is ->'+count);
+
+count++;
+
+console.log('The value of count is ->'+count);
