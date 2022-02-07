c6d19c2fb4c13e6e902fbabb2f175d03e5957a8d
unknown <mail.ahliwasum@gmail.com>
2022-02-07 11:41:57 +0530

    count display

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-alert('I am Javascript')
-console.log('This is also javascript');
+//alert("I am Javascript");
+console.log("This is also javascript");
 
-var count=0;
+var count = 0;
 
-count=count+1;
+// count=count+1;
 
-console.log('The value of count is ->'+count);
+// console.log('The value of count is ->'+count);
 
-count++;
+// count++;
 
-console.log('The value of count is ->'+count);
+// console.log('The value of count is ->'+count);
+
+//document.write('Filally from Here!')
+
+function countDisplay() {
+  count = count + 1;
+
+  console.log("The value of count is ->" + count);
+
+  count++;
+
+  console.log("The value of count is ->" + count);
+}
+
+//countDisplay();
