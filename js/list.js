03af928508857861629aa2f897ab28a4831712d0
unknown <mail.ahliwasum@gmail.com>
2022-02-10 12:00:36 +0530

    listing is completed

---
+++ js/list.js
@@ -0,0 +1,39 @@
+//GLOBAL Function
+fetch('../assets/cars.json') 
+.then(response => response.json())
+.then(jsonCars =>{
+    console.log(jsonCars);
+    localStorage.setItem('CAR_LIST',JSON.stringify(jsonCars));
+
+    let initPosition=1;
+    var table = document.getElementById("carsTable");
+    for(let car of jsonCars)
+    {
+        var row = table.insertRow(initPosition);
+        row.insertCell(0).innerHTML= car.id;
+        row.insertCell(1).innerHTML= car.name;
+        row.insertCell(2).innerHTML= car.category;
+        row.insertCell(3).innerHTML= car.manufacture;
+        row.insertCell(4).innerHTML= car.price;
+        row.insertCell(5).innerHTML='<div class="btn-group"><button type="button" onclick="editCar('+car.id+')" class="btn-xs btn-warning">Edit</button><button type="button" onClick="deleteCar('+car.id+')" class="btn-xs btn-danger">Delete</button></div>';
+        initPosition++;
+    }  
+} );
+
+function createCar()
+{
+     window.location.href='create.html';
+}
+
+function deleteCar(id)
+{
+    localStorage.setItem('SELECTED_ID',id);
+    window.location.href="delete.html";
+}
+function editCar(id)
+{
+    localStorage.setItem('SELECTED_ID',id);
+    window.location.href="edit.html";
+}
+
+
