6d55515b34e57dc2317ff4d870bff6b84299cdc2
unknown <mail.ahliwasum@gmail.com>
2022-02-15 09:57:24 +0530

    delete functionality completed

--- js/delete.js
+++ js/delete.js
@@ -1,4 +1,42 @@
-function redirectIndex(){
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        var carId = localStorage.getItem('SELECTED_ID');
+        var carList=JSON.parse(localStorage.getItem('CAR_LIST'));
+        var editCar= carList.find(c=>c.id==carId);
+        console.log(editCar)
+        document.getElementById('carId').value=editCar.id;    
+        document.getElementById('carName').value=editCar.name;
+        document.getElementById('carDate').value=editCar.manufacture;
+        document.getElementById('carCategory').value=editCar.category;
+        document.getElementById('carPrice').value=editCar.price;
+
+    }
+}//load data
+
+// name:
+// category:document.getElementById('carCategory').value,
+// manufacture:document.getElementById('carDate').value,
+// price:document.getElementById('carPrice').value,
+
+
+function onSubmitForm()
+{
+    console.log("I m in delete");
+
+    var carList=JSON.parse(localStorage.getItem('CAR_LIST'));
+    var seletedCarId=  localStorage.getItem('SELECTED_ID');
+    const tempCarList = carList.filter((item) => item.id != seletedCarId);
+
+
+    localStorage.setItem('CAR_LIST',JSON.stringify(tempCarList)); 
+
+    console.log(tempCarList);
     window.location.href="index.html";
 
+}
+
+
+function redirectIndex(){
+    window.location.href="index.html";
 }
--- js/list.js
+++ js/list.js
@@ -1,44 +1,41 @@
 //GLOBAL Function
-fetch('../assets/cars.json') 
-.then(response => response.json())
-.then(jsonCars =>{
-    if(localStorage.key('CAR_LIST'))
-    {
-        jsonCars=JSON.parse(localStorage.getItem('CAR_LIST')); 
+fetch("../assets/cars.json")
+  .then((response) => response.json())
+  .then((jsonCars) => {
+    if (localStorage.key("CAR_LIST")) {
+      jsonCars = JSON.parse(localStorage.getItem("CAR_LIST"));
+    } else {
+      console.log("Loaded......");
+      localStorage.setItem("CAR_LIST", JSON.stringify(jsonCars));
     }
-    else{
-        console.log('Loaded......');
-        localStorage.setItem('CAR_LIST',JSON.stringify(jsonCars)); 
-    }
-    let initPosition=1;
+    let initPosition = 1;
     var table = document.getElementById("carsTable");
-    for(let car of jsonCars)
-    {
-        var row = table.insertRow(initPosition);
-        row.insertCell(0).innerHTML= car.id;
-        row.insertCell(1).innerHTML= car.name;
-        row.insertCell(2).innerHTML= car.category;
-        row.insertCell(3).innerHTML= car.manufacture;
-        row.insertCell(4).innerHTML= car.price;
-        row.insertCell(5).innerHTML='<div class="btn-group"><button type="button" onclick="editCar('+car.id+')" class="btn-xs btn-warning">Edit</button><button type="button" class="btn-xs btn-danger" data-toggle="modal" data-target="#myModal">Delete</button></div>';
-        initPosition++;
-    } 
-} );
+    for (let car of jsonCars) {
+      var row = table.insertRow(initPosition);
+      row.insertCell(0).innerHTML = car.id;
+      row.insertCell(1).innerHTML = car.name;
+      row.insertCell(2).innerHTML = car.category;
+      row.insertCell(3).innerHTML = car.manufacture;
+      row.insertCell(4).innerHTML = car.price;
+      row.insertCell(5).innerHTML =
+        '<b<div class="btn-group"><button type="button" onclick="editCar(' +
+        car.id +
+        ')" class="btn-xs btn-warning"><i class="fa fa-pencil" aria-hidden="true"></i></button><button type="button" onclick="deleteCar(' +
+        car.id +
+        ')" class="btn-xs btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></button></div>';
+      initPosition++;
+    }
+  });
 
-function createCar()
-{
-     window.location.href='create.html';
+function createCar() {
+  window.location.href = "create.html";
 }
 
-function deleteCar(id)
-{
-    localStorage.setItem('SELECTED_ID',id);
-    window.location.href="delete.html";
+function deleteCar(id) {
+  localStorage.setItem("SELECTED_ID", id);
+  window.location.href = "delete.html";
 }
-function editCar(id)
-{
-    localStorage.setItem('SELECTED_ID',id);
-    window.location.href="edit.html";
+function editCar(id) {
+  localStorage.setItem("SELECTED_ID", id);
+  window.location.href = "edit.html";
 }
-
-
