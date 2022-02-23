1b7d71ae0f5dbd4b96d7962fd07f1c9908233a49
unknown <mail.ahliwasum@gmail.com>
2022-02-23 21:06:48 +0530

    calculation done

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 
 
-function oneClicked()
+function displayNumber(num)
 {
   var calString= document.getElementById('calc').value;
+  document.getElementById('calc').value=calString+num;
   console.log(calString);
   //if(calString)
   
+}
+
+
+function onClear()
+{
+  document.getElementById('calc').value='';
+}
+
+function onCalculate()
+{
+  var calculateString= document.getElementById('calc').value;
+
+  if(calculateString.includes('+'))
+  {
+    var result1= parseInt(calculateString.split('+')[0]);
+    var result2= parseInt(calculateString.split('+')[1]);
+    document.getElementById('calc').value=result1+result2;
+  }
+  else if(calculateString.includes('-'))
+  {
+    var result1= parseInt(calculateString.split('-')[0]);
+    var result2= parseInt(calculateString.split('-')[1]);
+    document.getElementById('calc').value=result1-result2;
+  }
+  else if(calculateString.includes('*'))
+  {
+    var result1= parseInt(calculateString.split('*')[0]);
+    var result2= parseInt(calculateString.split('*')[1]);
+    document.getElementById('calc').value=result1*result2;
+  }
+  else if(calculateString.includes('/'))
+  {
+    var result1= parseInt(calculateString.split('/')[0]);
+    var result2= parseInt(calculateString.split('/')[1]);
+    document.getElementById('calc').value=result1/result2;
+  }
+
+
 }
